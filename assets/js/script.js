18e12a5beb1ca813d51fa393bb9cfeec2f4d2c8f
Reymar Enteria <enteriarv@gmail.com>
2019-05-28 18:56:16 +0800

    WebPortfolio

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+        document.getElementById("navbar").style.backgroundColor = "#b20a2c";
+    } else if(prevScrollpos == 0) {
+        document.getElementById("navbar").style.backgroundColor = "transparent";
+    }else{
+        document.getElementById("navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
+$(".bar").each(function () {
+    $(this).find(".bar-inner").animate({
+        width: $(this).attr("data-width")
+    }, 2000)
+});
